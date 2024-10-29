3221befef34145c76e7294a0498e14dc2c35d032
Micheal Jones <micheal@mcjones.ca>
2024-10-28 20:22:52 -0600

    Give some indication that the export is working as it's much slower on Sequoia

--- bear_export_sync.py
+++ bear_export_sync.py
@@ -220,7 +220,9 @@ draft: false
 
             for filepath in file_list:
                 note_count += 1
-                # print(filepath)
+                #print(filepath)
+                if note_count % 100 == 0:
+                    print('.')
                 if export_as_textbundles:
                     if check_image_hybrid(md_text):
                         make_text_bundle(md_text, filepath, mod_dt)
